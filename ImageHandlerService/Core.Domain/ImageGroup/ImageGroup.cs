1c5951ee69aa992c9362d71ad064790066b31b80
SagiKPJH1021 <Juhyung1021@gmail.com>
2024-04-23 12:23:00 +0900

    feat : add Core

--- ImageHandlerService/Core.Domain/ImageGroup/ImageGroup.cs
+++ ImageHandlerService/Core.Domain/ImageGroup/ImageGroup.cs
@@ -1,4 +1,5 @@
-﻿using Core.Domain.ValueObject;
+﻿using Core.Domain.Service;
+using Core.Domain.ValueObject;
 using ImageHandlerService.Core.Domain.ValueObject;
 
 namespace Core.Domain;
@@ -8,4 +9,6 @@ public class ImageGroup : Detail
     public ID Id { get; set; } = null!;
     public Name Name { get; set; } = new(string.Empty);
     public List<Image> Images { get; set;} = [];
+
+    public List<IService> Services { get; set; } = [];
 }
---
+++ ImageHandlerService/Core.Domain/Service/ServiceInfo.cs
@@ -0,0 +1,12 @@
+﻿using Core.Domain.ValueObject;
+
+namespace Core.Domain.Service;
+
+public class ServiceInfo : Detail
+{
+    public ID Id { get; set; } = new(0);
+
+    public List<Image> Images { get; set; } = [];
+    public List<ImageGroup> ImageGroups { get; set; } = [];
+    public List<IService> Services { get; set; } = [];
+}
