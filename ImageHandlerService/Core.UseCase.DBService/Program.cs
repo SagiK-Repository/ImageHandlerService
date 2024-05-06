274d0c56453ca550bcadcc02094c17c740114400
SagiKPJH1021 <Juhyung1021@gmail.com>
2024-05-06 12:33:11 +0900

    feat : add MediatR and Repository

--- ImageHandlerService/Core.UseCase.DBService/Program.cs
+++ ImageHandlerService/Core.UseCase.DBService/Program.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.EntityFrameworkCore;
+﻿using Feature.DataBase.Context;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 using Service.DBService.Extension;
 
 Console.WriteLine("This is UseCase DBService!");
@@ -7,6 +8,8 @@ Console.WriteLine("This is UseCase DBService!");
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDBContext(builder.Configuration);
+builder.Services.AddRepository();
+builder.Services.AddMediatR(configuration => configuration.RegisterServicesFromAssemblies(typeof(ImageHandlerDBContext).Assembly));
 
 var app = builder.Build();
 
