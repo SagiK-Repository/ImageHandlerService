24e3331f2932f211b3c43a373b266c6b724d3e3b
SagiKPJH1021 <Juhyung1021@gmail.com>
2024-06-21 05:11:51 +0900

    feat: add iserializer

---
+++ Core.UseCase.Serializer/Serializer/ISerializer.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.UseCase.Serializer.Serializer;
+
+public interface ISerializer
+{
+    object Deserialize(string target, Type targetType);
+    object Serialize(object input, Type targetType);
+
+    Task<object> DeserializeAsync(string target, Type targetType);
+    Task<object> SerializeAsync(object value, Type targetType);
+}
